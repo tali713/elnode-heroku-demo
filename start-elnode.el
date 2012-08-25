6c6f97cc9452a12f35d8b935a75df2af69d4d380
Evan Izaksonas-Smith <izak0002@umn.edu>
2012-08-25 04:37:22 -0500

    Initial Update * start-elnode.el: primary file * esxml: as support

--- start-elnode.el
+++ start-elnode.el
@@ -1,4 +1,5 @@
 ;; -*- lexical-binding: t -*-
+(require 'htmlfontify)
 (setq package-archives
       '(("gnu" . "http://elpa.gnu.org/packages/")
         ("marmalade" . "http://marmalade-repo.org/packages/")))
@@ -20,16 +21,32 @@
 (package-install 'elnode)
 (message "elnode installed")
 
+(setq base-dir default-directory)
+(load-file (concat base-dir "esxml/esxml.el"))
 (defun handler (httpcon)
   "Demonstration function"
   (elnode-http-start httpcon "200"
                      '("Content-type" . "text/html")
                      `("Server" . ,(concat "GNU Emacs " emacs-version)))
   (elnode-http-return httpcon
-                      "<html><body><h1>Hello from EEEMACS.</h1></body></html>"))
+    (esxml-demo httpcon)))
 
-;;(elnode-start 'handler :port elnode-init-port :host elnode-init-host)
-(elnode-init)
+(let ((count 0))
+  (defun sxml-demo (httpcon)
+    (incf count)
+    (case (intern (elnode-http-pathinfo httpcon))            
+      (/messages (with-current-buffer "*Messages*" (sxml-to-xml `(html (body (pre ,(buffer-string)))))))
+      
+      (t (sxml-to-xml
+          `(html
+            (body
+             (h1 "Hello from Emacs!") (br)
+             "Trying to visit " ,(format "%s" (elnode-http-pathinfo httpcon)) (br)
+             "Visit " (a (@ (href "/messages")) "messages") " to see the *Messages* buffer." (br)
+             "Have been visited " ,(format "%s" count) " times since last started.")))))))
+
+(elnode-start 'handler :port elnode-init-port :host elnode-init-host)
+;;(elnode-init)
 
 (while t
   (accept-process-output nil 1))
